e954040b97eafee87a3c156ba2894edf5faf4a1b
nguyenhamy2000.td@gmail.com <nguyenhamy2000.td@gmail.com>
2026-04-09 21:29:01 +0700

    [Create][UI] login

---
+++ job-frontend/src/pages/login/login.jsx
@@ -0,0 +1,43 @@
+import styles from "./login.module.css";
+import logo from "../../assets/image/logo.png";
+import pic2 from "../../assets/image/pic2.png";
+export default function Login() {
+  return (
+    <div className={styles.container}>
+      <img src={pic2} className={styles.pic1} />
+      <div className={styles.card}>
+        <div className={styles.logo}>
+          <img src={logo} className={styles.logoIcon} />
+          <span>MyCV</span>
+        </div>
+
+        <h2 className={styles.title}>Đăng nhập</h2>
+
+        <div className={styles.form}>
+          <label>Email</label>
+          <input type="text" placeholder="username@gmail.com" />
+
+          <label>Mật khẩu</label>
+          <input type="password" placeholder="abc@123" />
+
+          <div className={styles.forgot}>Quên mật khẩu ?</div>
+
+          <button className={styles.loginBtn}>Đăng nhập</button>
+
+          <div className={styles.or}>hoặc tiếp tục với </div>
+
+          <button className={styles.googleBtn}>
+            <img
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              alt=""
+            />
+          </button>
+
+          <div className={styles.register}>
+            Bạn chưa có tài khoản ?<span> Đăng ký ngay</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
