20bbd14c85a512693d80732bf92fbf996fd5fb2d
tramy984 <nguyenthitramy9804@gmail.com>
2026-04-11 09:44:26 +0700

    feat : add Homepage Recuiter UI

--- job-frontend/src/App.jsx
+++ job-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage/Homepage";
 import Login from "./pages/login/login";
 import RegisterChoicePage from "./pages/register/registerChoice";
 import RegisterPage from "./pages/register/register";
+import HomepageRecuiter from "./pages/Homepage_Recuiter/Homepage_Recuiter";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function App() {
          <Route path="/login" element={<Login />} />
         <Route path="/registerChoice" element={<RegisterChoicePage />} />
         <Route path="/register/:role" element={<RegisterPage />} />
+        <Route path="/homepage-recuiter" element={<HomepageRecuiter />} />
       </Routes>
     </BrowserRouter>
   );
